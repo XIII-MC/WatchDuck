84a0f89a13e9bc37ca9f54e2147caa3d722559a4
XIII___ <cyjallet@gmail.com>
2022-05-01 20:14:36 +0200

    Added Jump A

---
+++ src/main/java/com/xiii/watchduck/check/checks/jump/JumpA.java
@@ -0,0 +1,24 @@
+package com.xiii.watchduck.check.checks.jump;
+
+import com.xiii.watchduck.check.Category;
+import com.xiii.watchduck.check.Check;
+import com.xiii.watchduck.check.CheckInfo;
+import com.xiii.watchduck.exempt.ExemptType;
+import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
+
+@CheckInfo(name = "Jump A", category = Category.MOVEMENT)
+public class JumpA extends Check {
+
+    int jumpTicks;
+
+    public void onMove(PacketPlayReceiveEvent packet, double motionX, double motionY, double motionZ, double lastmotionX, double lastmotionY, double lastmotionZ, float deltaYaw, float deltaPitch, float lastdeltaYaw, float lastdeltaPitch) {
+        boolean exempt = isExempt(ExemptType.FLYING, ExemptType.TELEPORT, ExemptType.WEB, ExemptType.SLAB, ExemptType.STAIRS);
+        if(motionY > 0) {
+            jumpTicks++;
+            if(jumpTicks == 1) {
+                if(motionY != 0.41999998688697815 && !exempt) fail("Invalid Jump", "my=" + motionY);
+            }
+            if(motionY == 0.42 && !exempt) removeBuffer();
+        } else jumpTicks = 0;
+    }
+}
