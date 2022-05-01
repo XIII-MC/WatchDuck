2faf3f2609a2604b31538149068642492b894659
XIII___ <cyjallet@gmail.com>
2022-05-01 20:12:45 +0200

    Added BadPacket B

---
+++ src/main/java/com/xiii/watchduck/check/checks/badpacket/BadPacketB.java
@@ -0,0 +1,23 @@
+package com.xiii.watchduck.check.checks.badpacket;
+
+import com.xiii.watchduck.check.Category;
+import com.xiii.watchduck.check.Check;
+import com.xiii.watchduck.check.CheckInfo;
+import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
+import io.github.retrooper.packetevents.packettype.PacketType;
+
+@CheckInfo(name = "BadPacket B", category = Category.PLAYER)
+public class BadPacketB extends Check {
+
+    int pCount;
+
+    public void onPacket(PacketPlayReceiveEvent packet) {
+        if(packet.getPacketId() == PacketType.Play.Client.FLYING) {
+            pCount++;
+        } else {
+            pCount = 0;
+        }
+        if(pCount > 42) fail("Packet spam", "pCount=" + pCount);
+
+    }
+}
