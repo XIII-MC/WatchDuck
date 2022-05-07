6f970072e2a2507702e5d711aa888663c9da890f
XIII___ <cyjallet@gmail.com>
2022-05-07 21:34:20 +0200

    Added Tower A

---
+++ src/main/java/com/xiii/watchduck/check/checks/scaffold/TowerA.java
@@ -0,0 +1,27 @@
+package com.xiii.watchduck.check.checks.scaffold;
+
+import com.xiii.watchduck.check.Category;
+import com.xiii.watchduck.check.Check;
+import com.xiii.watchduck.check.CheckInfo;
+import com.xiii.watchduck.exempt.ExemptType;
+import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
+import io.github.retrooper.packetevents.packettype.PacketType;
+import org.bukkit.Bukkit;
+
+@CheckInfo(name = "Tower A", category = Category.WORLD)
+public class TowerA extends Check {
+
+    int bCount;
+
+    public void onPacket(PacketPlayReceiveEvent packet) {
+        boolean exempt = isExempt(ExemptType.CLIMBABLE, ExemptType.FLYING);
+        if (packet.getPacketId() == PacketType.Play.Client.BLOCK_PLACE) {
+            if (data.motionY > 0.2) {
+                bCount++;
+                if(bCount > 4 && !exempt) fail("Placed too many blocks", "bCount=" + bCount);
+                if(bCount > 4 && !exempt) packet.setCancelled(true);
+            } else bCount = 0;
+
+        }
+    }
+}
