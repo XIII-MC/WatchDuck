31a6b172273f0245624510df2512a801bdacbd22
XIII___ <cyjallet@gmail.com>
2022-05-14 11:30:48 +0200

    Improved Jump A

--- src/main/java/com/xiii/watchduck/check/checks/jump/JumpA.java
+++ src/main/java/com/xiii/watchduck/check/checks/jump/JumpA.java
@@ -12,11 +12,11 @@ public class JumpA extends Check {
     int jumpTicks;
 
     public void onMove(PacketPlayReceiveEvent packet, double motionX, double motionY, double motionZ, double lastmotionX, double lastmotionY, double lastmotionZ, float deltaYaw, float deltaPitch, float lastdeltaYaw, float lastdeltaPitch) {
-        boolean exempt = isExempt(ExemptType.FLYING, ExemptType.TELEPORT, ExemptType.WEB, ExemptType.SLAB, ExemptType.STAIRS, ExemptType.LIQUID, ExemptType.BLOCK_ABOVE, ExemptType.VELOCITY, ExemptType.PLACE);
+        boolean exempt = isExempt(ExemptType.FLYING, ExemptType.TELEPORT, ExemptType.WEB, ExemptType.SLAB, ExemptType.STAIRS, ExemptType.LIQUID, ExemptType.BLOCK_ABOVE, ExemptType.VELOCITY, ExemptType.PLACE, ExemptType.STAIRS, ExemptType.SLAB, ExemptType.CLIMBABLE, ExemptType.SLIME, ExemptType.NEAR_VEHICLE);
         if(motionY > 0) {
             jumpTicks++;
-            if(jumpTicks == 1) {
-                if(motionY != 0.41999998688697815 && !exempt && !data.onLowBlock) fail("Invalid Jump", "my=" + motionY);
+            if(jumpTicks == 1 && !data.onLowBlock) {
+                if(motionY != 0.41999998688697815 && !exempt && motionY != 0.5) fail("Invalid Jump", "my=" + motionY);
                 if(motionY == 0.41999998688697815) removeBuffer();
             }
         } else jumpTicks = 0;
