f6ff9c1b8cd511adfe26e924a77193160b8d68e0
XIII___ <cyjallet@gmail.com>
2022-05-15 22:21:41 +0200

    Improved vClip A

--- src/main/java/com/xiii/watchduck/check/checks/vclip/vClipA.java
+++ src/main/java/com/xiii/watchduck/check/checks/vclip/vClipA.java
@@ -12,6 +12,6 @@ public class vClipA extends Check {
 
     public void onMove(PacketPlayReceiveEvent packet, double motionX, double motionY, double motionZ, double lastmotionX, double lastmotionY, double lastmotionZ, float deltaYaw, float deltaPitch, float lastdeltaYaw, float lastdeltaPitch) {
         boolean exempt = isExempt(ExemptType.FLYING, ExemptType.SLIME, ExemptType.TELEPORT, ExemptType.JOINED, ExemptType.VELOCITY, ExemptType.SLAB, ExemptType.STAIRS, ExemptType.NEAR_VEHICLE);
-        if(motionY > 0.6 + (data.player.hasPotionEffect(PotionEffectType.JUMP) ? ((data.player.getPotionEffect(PotionEffectType.JUMP).getAmplifier() + 1) * 0.1) : 0) && !exempt) fail("Teleported Upwards", motionY);
+        if(motionY > 0.6 + (data.player.hasPotionEffect(PotionEffectType.JUMP) ? ((data.getPotionEffectAmplifier(PotionEffectType.JUMP) * 0.1) + 1) : 0) && !exempt) fail("Teleported Upwards", motionY);
     }
 }
