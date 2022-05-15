c0327cce8842b7f99fec92228b1e92c78c1e5911
XIII___ <cyjallet@gmail.com>
2022-05-15 08:08:02 +0200

    Improved BadPacket E

--- src/main/java/com/xiii/watchduck/check/checks/badpacket/BadPacketE.java
+++ src/main/java/com/xiii/watchduck/check/checks/badpacket/BadPacketE.java
@@ -8,7 +8,7 @@ import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
 @CheckInfo(name = "BadPacket E", category = Category.PLAYER)
 public class BadPacketE extends Check {
 
-    public void onMove(PacketPlayReceiveEvent packet, double motionX, double motionY, double motionZ, double lastmotionX, double lastmotionY, double lastmotionZ, float deltaYaw, float deltaPitch, float lastdeltaYaw, float lastdeltaPitch) {
+    public void onPacket(PacketPlayReceiveEvent packet) {
         if (data.eatDelay < 1400) {
             fail("Ate too fast", "delay=" + data.eatDelay);
             data.eatDelay = 1400;
