8d2c510dfaf56a12e4f80eefa89c2152deded1fb
XIII___ <cyjallet@gmail.com>
2022-05-15 23:16:01 +0200

    Removed BadPacket H

--- src/main/java/com/xiii/watchduck/check/checks/badpacket/BadPacketH.java
+++ src/main/java/com/xiii/watchduck/check/checks/badpacket/BadPacketH.java
@@ -5,16 +5,18 @@ import com.xiii.watchduck.check.Check;
 import com.xiii.watchduck.check.CheckInfo;
 import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
 import io.github.retrooper.packetevents.packettype.PacketType;
+import org.bukkit.Bukkit;
 
 @CheckInfo(name = "BadPacket H", category = Category.COMBAT)
 public class BadPacketH extends Check {
 
+    double lastDelay;
+
     public void onPacket(PacketPlayReceiveEvent packet) {
+        //Bukkit.broadcastMessage("Delay=" + ((data.lastWindow - data.lastEntityUse) - lastDelay));
         if(packet.getPacketId() == PacketType.Play.Client.WINDOW_CLICK) data.lastWindow = System.currentTimeMillis();
         if(packet.getPacketId() == PacketType.Play.Client.USE_ENTITY) data.lastEntityUse = System.currentTimeMillis();
-        if(data.lastEntityUse - data.lastWindow < 300 && data.lastEntityUse - data.lastWindow > -200) fail("Packet Spam", "delay=" + (data.lastEntityUse - data.lastWindow));
-        if(data.lastEntityUse - data.lastWindow > 300 && data.lastEntityUse - data.lastWindow < -200) removeBuffer();
-        data.lastEntityUse = 1000;
-        data.lastWindow = 0;
+        lastDelay = data.lastWindow - data.lastEntityUse;
+
     }
 }
