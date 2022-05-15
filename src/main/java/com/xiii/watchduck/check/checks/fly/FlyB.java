761d4ed75cd5cd4d3ac4a5e3d55831921d6fdb49
XIII___ <cyjallet@gmail.com>
2022-05-15 08:08:17 +0200

    Improved Fly B

--- src/main/java/com/xiii/watchduck/check/checks/fly/FlyB.java
+++ src/main/java/com/xiii/watchduck/check/checks/fly/FlyB.java
@@ -16,8 +16,8 @@ public class FlyB extends Check {
         boolean exempt = isExempt(ExemptType.FLYING, ExemptType.SLIME, ExemptType.TELEPORT, ExemptType.JOINED, ExemptType.INSIDE_VEHICLE, ExemptType.NEAR_VEHICLE, ExemptType.CLIMBABLE, ExemptType.LIQUID, ExemptType.PLACE, ExemptType.STAIRS, ExemptType.SLAB);
         airLimit = 6;
         if(motionY > 0.05 && !exempt) airTicks++;
-        if(isExempt(ExemptType.VELOCITY)) airLimit += 2;
+        if(isExempt(ExemptType.VELOCITY)) airLimit += 5;
         if(airTicks > airLimit && !exempt) fail("Added Airticks", "ticks=" + airTicks);
-        if(motionY <= 0.05 && data.onSolidGround) airTicks = 0;
+        if(motionY <= 0.05 && data.onSolidGround || data.ground2() || data.isOnGround()) airTicks = 0;
     }
 }
