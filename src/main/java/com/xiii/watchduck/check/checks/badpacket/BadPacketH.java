66e69c4c1ad93c07066d7b65269edc9c0aefe996
Vagdedes2 <ickbinbesser12@gmail.com>
2022-05-16 20:57:44 +0200

    Recoded BadPacket H

--- src/main/java/com/xiii/watchduck/check/checks/badpacket/BadPacketH.java
+++ src/main/java/com/xiii/watchduck/check/checks/badpacket/BadPacketH.java
@@ -10,13 +10,20 @@ import org.bukkit.Bukkit;
 @CheckInfo(name = "BadPacket H", category = Category.COMBAT)
 public class BadPacketH extends Check {
 
-    double lastDelay;
+    double lastWindowTime;
+    double lastUseEntityTime;
 
     public void onPacket(PacketPlayReceiveEvent packet) {
-        //Bukkit.broadcastMessage("Delay=" + ((data.lastWindow - data.lastEntityUse) - lastDelay));
-        if(packet.getPacketId() == PacketType.Play.Client.WINDOW_CLICK) data.lastWindow = System.currentTimeMillis();
-        if(packet.getPacketId() == PacketType.Play.Client.USE_ENTITY) data.lastEntityUse = System.currentTimeMillis();
-        lastDelay = data.lastWindow - data.lastEntityUse;
+        //
+        if(packet.getPacketId() == PacketType.Play.Client.WINDOW_CLICK) lastWindowTime = System.currentTimeMillis();
+        if(packet.getPacketId() == PacketType.Play.Client.USE_ENTITY) lastUseEntityTime = System.currentTimeMillis();
+        //lastDelay = data.lastWindow - data.lastEntityUse;
+        double delay = lastUseEntityTime - lastWindowTime;
+        if(packet.getPacketId() == PacketType.Play.Client.USE_ENTITY) {
+            if(delay < 50) {
+                fail("Clicked Inventory", delay);
+            }
+        }
 
     }
 }
--- src/main/java/com/xiii/watchduck/check/checks/killaura/KillAuraC.java
+++ src/main/java/com/xiii/watchduck/check/checks/killaura/KillAuraC.java
@@ -5,12 +5,27 @@ import com.xiii.watchduck.check.Check;
 import com.xiii.watchduck.check.CheckInfo;
 import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
 import io.github.retrooper.packetevents.packettype.PacketType;
+import io.github.retrooper.packetevents.packetwrappers.play.in.useentity.WrappedPacketInUseEntity;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
 
 @CheckInfo(name = "KillAura C", category = Category.COMBAT)
 public class KillAuraC extends Check {
 
-    int cCount;
+    Entity lastTarget;
+    int targetedEntities;
 
     public void onPacket(PacketPlayReceiveEvent packet) {
+        if(packet.getPacketId() == PacketType.Play.Client.USE_ITEM) {
+            WrappedPacketInUseEntity ue = new WrappedPacketInUseEntity(packet.getNMSPacket());
+            if(lastTarget == null) lastTarget = ue.getEntity();
+            if(ue.getEntity() != lastTarget) {
+                ++targetedEntities;
+            }
+            lastTarget = ue.getEntity();
+            if(targetedEntities > 1) {
+                Bukkit.broadcastMessage("TargetCount: " + targetedEntities);
+            }
+        }
     }
 }
