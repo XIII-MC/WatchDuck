69fa10cadfa4bd89fe2ca70ae348814b055dfb91
Vagdedes2 <ickbinbesser12@gmail.com>
2022-05-16 21:49:12 +0200

    Recoded Killaura C

--- src/main/java/com/xiii/watchduck/check/checks/killaura/KillAuraC.java
+++ src/main/java/com/xiii/watchduck/check/checks/killaura/KillAuraC.java
@@ -13,19 +13,20 @@ import org.bukkit.entity.Entity;
 public class KillAuraC extends Check {
 
     Entity lastTarget;
+    Entity target;
     int targetedEntities;
 
     public void onPacket(PacketPlayReceiveEvent packet) {
-        if(packet.getPacketId() == PacketType.Play.Client.USE_ITEM) {
+        if(packet.getPacketId() == PacketType.Play.Client.USE_ENTITY) {
             WrappedPacketInUseEntity ue = new WrappedPacketInUseEntity(packet.getNMSPacket());
-            if(lastTarget == null) lastTarget = ue.getEntity();
-            if(ue.getEntity() != lastTarget) {
-                ++targetedEntities;
+            lastTarget = target == null ? ue.getEntity() : target;
+            target = ue.getEntity();
+            if(target != lastTarget) {
+                targetedEntities++;
             }
-            lastTarget = ue.getEntity();
-            if(targetedEntities > 1) {
-               // Bukkit.broadcastMessage("TargetCount: " + targetedEntities);
-            }
+            if(targetedEntities > 1)
+                fail("Multi Aura", targetedEntities);
+
         }
         if(packet.getPacketId() == PacketType.Play.Client.POSITION || packet.getPacketId() == PacketType.Play.Client.POSITION_LOOK || packet.getPacketId() == PacketType.Play.Client.LOOK) {
             targetedEntities = 0;
