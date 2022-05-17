bd0822279ba83db31fb0f68f93c17aa53dd90d58
Vagdedes2 <ickbinbesser12@gmail.com>
2022-05-17 19:52:33 +0200

    Recoded Jump A

--- src/main/java/com/xiii/watchduck/check/checks/jump/JumpA.java
+++ src/main/java/com/xiii/watchduck/check/checks/jump/JumpA.java
@@ -5,20 +5,21 @@ import com.xiii.watchduck.check.Check;
 import com.xiii.watchduck.check.CheckInfo;
 import com.xiii.watchduck.exempt.ExemptType;
 import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
+import org.bukkit.potion.PotionEffectType;
 
 @CheckInfo(name = "Jump A", category = Category.MOVEMENT)
 public class JumpA extends Check {
 
-    int jumpTicks;
-
     public void onMove(PacketPlayReceiveEvent packet, double motionX, double motionY, double motionZ, double lastmotionX, double lastmotionY, double lastmotionZ, float deltaYaw, float deltaPitch, float lastdeltaYaw, float lastdeltaPitch) {
-        boolean exempt = isExempt(ExemptType.FLYING, ExemptType.TELEPORT, ExemptType.WEB, ExemptType.SLAB, ExemptType.STAIRS, ExemptType.LIQUID, ExemptType.BLOCK_ABOVE, ExemptType.VELOCITY, ExemptType.PLACE, ExemptType.STAIRS, ExemptType.SLAB, ExemptType.CLIMBABLE, ExemptType.SLIME, ExemptType.NEAR_VEHICLE);
-        if(motionY > 0) {
-            jumpTicks++;
-            if(jumpTicks == 1 && !data.onLowBlock) {
-                if(motionY != 0.41999998688697815 && !exempt && motionY != 0.5) fail("Invalid Jump", "my=" + motionY);
-                if(motionY == 0.41999998688697815) removeBuffer();
-            }
-        } else jumpTicks = 0;
+        boolean step = mathOnGround(motionY) && mathOnGround(data.from.getY());
+        boolean jumped = motionY > 0 && data.from.getY() % (1D/64) == 0 && !data.playerGround && !step;
+        double expectedJumpMotion = 0.42F + (data.getPotionEffectAmplifier(PotionEffectType.JUMP) * 0.1F);
+        boolean exempt = isExempt(ExemptType.INSIDE_VEHICLE, ExemptType.VELOCITY, ExemptType.CLIMBABLE, ExemptType.FLYING, ExemptType.SLIME, ExemptType.BLOCK_ABOVE, ExemptType.PISTON, ExemptType.LIQUID, ExemptType.NEAR_VEHICLE, ExemptType.TELEPORT, ExemptType.WEB, ExemptType.TRAPDOOR);
+        double diff = Math.abs(motionY - expectedJumpMotion);
+        if(jumped && !exempt) {
+            if (diff > 0.2 && motionY < expectedJumpMotion) {
+                fail("Invalid Jump", "my=" + motionY);
+            }else removeBuffer();
+        }
     }
 }
