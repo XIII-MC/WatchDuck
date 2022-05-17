79f3e9d1e6b540e7e147a1563d19e9413bd3acc7
XIII___ <cyjallet@gmail.com>
2022-05-17 22:44:21 +0200

    Improved Crasher B

--- src/main/java/com/xiii/watchduck/check/checks/crasher/CrasherB.java
+++ src/main/java/com/xiii/watchduck/check/checks/crasher/CrasherB.java
@@ -3,15 +3,27 @@ package com.xiii.watchduck.check.checks.crasher;
 import com.xiii.watchduck.check.Category;
 import com.xiii.watchduck.check.Check;
 import com.xiii.watchduck.check.CheckInfo;
-import com.xiii.watchduck.exempt.ExemptType;
 import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
+import io.github.retrooper.packetevents.packettype.PacketType;
+import io.github.retrooper.packetevents.packetwrappers.play.in.chat.WrappedPacketInChat;
+import io.github.retrooper.packetevents.packetwrappers.play.in.clientcommand.WrappedPacketInClientCommand;
 
 @CheckInfo(name = "Crasher B", category = Category.PLAYER)
 public class CrasherB extends Check {
 
-    public void onMove(PacketPlayReceiveEvent packet, double motionX, double motionY, double motionZ, double lastmotionX, double lastmotionY, double lastmotionZ, float deltaYaw, float deltaPitch, float lastdeltaYaw, float lastdeltaPitch) {
-        boolean exempt = isExempt(ExemptType.TELEPORT);
-        if(data.getDistance(true) > 10 && !exempt) fail("Moved too fast between positions", "dis=" + data.getDistance(true));
-        if(data.getDistance(true) > 10 && !exempt) packet.setCancelled(true);
+    public void onPacket(PacketPlayReceiveEvent packet) {
+        if(packet.getPacketId() == PacketType.Play.Client.CHAT) {
+            WrappedPacketInChat chat = new WrappedPacketInChat(packet.getNMSPacket());
+            String message = chat.getMessage();
+            if(String.valueOf(message).contains("${")) fail("Log4J (or similar) Exploit", "NaN (Protection Reasons)");
+            if(String.valueOf(message).contains("${")) packet.setCancelled(true);
+        }
+        if(packet.getPacketId() == PacketType.Play.Client.CLIENT_COMMAND) {
+            WrappedPacketInClientCommand cmd = new WrappedPacketInClientCommand(packet.getNMSPacket());
+            String message = String.valueOf(cmd.getClientCommand());
+            if(String.valueOf(message).contains("${")) fail("Log4J (or similar) Exploit", "NaN (Protection Reasons)");
+            if(String.valueOf(message).contains("${")) packet.setCancelled(true);
+        }
     }
+
 }
