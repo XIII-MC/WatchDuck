0934318de5e33890f4f56287cc50e6d0abb34a39
Vagdedes2 <ickbinbesser12@gmail.com>
2022-05-20 23:36:47 +0200

    Added BadPacket I

---
+++ src/main/java/com/xiii/watchduck/check/checks/badpacket/BadPacketI.java
@@ -0,0 +1,50 @@
+package com.xiii.watchduck.check.checks.badpacket;
+
+import com.xiii.watchduck.check.Category;
+import com.xiii.watchduck.check.Check;
+import com.xiii.watchduck.check.CheckInfo;
+import com.xiii.watchduck.utils.SampleList;
+import io.github.retrooper.packetevents.event.impl.PacketPlayReceiveEvent;
+import io.github.retrooper.packetevents.packettype.PacketType;
+
+@CheckInfo(name = "BadPacket I", category = Category.PLAYER)
+public class BadPacketI extends Check{
+
+    long lastms = System.currentTimeMillis();
+    long lastPosition = System.currentTimeMillis();
+    double bal;
+    double lastbal;
+    SampleList<Double> balances = new SampleList<>(4);
+
+    public void onPacket(PacketPlayReceiveEvent packet) {
+        if(packet.getPacketId() == PacketType.Play.Client.FLYING) {
+            long diff = System.currentTimeMillis() - lastms;
+            long diff2 = System.currentTimeMillis() - lastPosition;
+            lastbal = bal;
+            bal += 50 - diff;
+            if(bal >= 5 && diff2 < 400) {
+                balances.add(bal);
+                if(balances.isCollected()) {
+                    if(balances.getAverageDouble(balances) >= 50) {
+                        fail("Basic Regen Check", "bal=" + bal);
+                    } else {
+                        removeBuffer();
+                    }
+                }
+                bal = 0;
+            }else {
+            }
+
+            lastms = System.currentTimeMillis();
+        }else {
+            if (packet.getPacketId() == PacketType.Play.Client.POSITION || packet.getPacketId() == PacketType.Play.Client.POSITION_LOOK || packet.getPacketId() == PacketType.Play.Client.LOOK) {
+                if(bal < -100) {
+                    bal = -20;
+                }
+                lastPosition = System.currentTimeMillis();
+            }
+        }
+
+
+    }
+}
